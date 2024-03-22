7c8bb86a7c93ccd26a93c57cec93b271705a2d03
Fraser Dempster <fraserdempster23@gmail.com>
2024-03-22 21:44:55 +0000

    added categories for grind 75 problems

---
+++ revisit/235_lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  // try both the iteration and recursion method
+  // the iteration is time O(n) and space O(1)
+  // recursion is time and space O(n)
+};
---
+++ revisit/704_binary-search.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  // check if nums is empty or null
+  // set left and right indexes
+  // left <= right
+  // set mid index
+  // if middle is elemnent then return mid index
+  // if nums[mid] is less than set left to mid + 1
+  // else right = mid - 1
+  // if nothing return -1
+};
