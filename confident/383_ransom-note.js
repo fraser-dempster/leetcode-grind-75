deec093455597d0799f8bb5ccbc8e672f155de12
Fraser Dempster <fraserdempster23@gmail.com>
2024-03-24 17:58:40 +0000

    add: various problem statements

---
+++ confident/383_ransom-note.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  // create a map for the magazine letters
+  // loop through the random note and decrement to check if char is in magazine map
+};
